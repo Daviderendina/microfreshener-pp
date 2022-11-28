a7af946c42cdbab82958eb031146794d1a0dd7e7
Davide <daviderendina9@gmail.com>
2022-11-28 18:16:47 +0100

    Added template

---
+++ k8s_template/service_template.py
@@ -0,0 +1,48 @@
+from project.kmodel.kService import KService
+
+# Template to which add metadata.name, metadata.labels, spec.ports, spec.selector
+SERVICE_CLUSTERIP_TEMPLATE = {
+    'api_version': 'v1',
+    'kind': 'Service',
+    'metadata': {
+        # 'name': TODO,
+        # 'namespace' : TODO
+    },
+    'spec': {
+        'ports': [
+            #{
+            #    'name': TODO,
+            #    'port': TODO,
+            #}
+        ],
+        'selector': {
+            #'label': 'value' TODO
+        }
+    }
+}
+#TODO fare domani un metodo che gli passa i parametri e genera il Service
+
+
+def generate_service_from_template(name: str, namespace: str, ports: list, selector_labels: dict):
+    service_dict = SERVICE_CLUSTERIP_TEMPLATE.copy()
+    service_dict["metadata"]["name"] = name
+    service_dict["metadata"]["namespace"] = namespace
+    service_dict["spec"]["ports"] = ports
+    service_dict["spec"]["selector"] = selector_labels
+
+    return KService.from_dict(service_dict)
+
+
+def generate_port_from_template(name: str, port: int, protocol: str = None, target_port: int = None) -> dict:
+    port = {
+        'name': name,
+        'port': port,
+    }
+
+    if protocol:
+        port['protocol'] = protocol
+
+    if target_port:
+        port['target_port'] = target_port
+
+    return port
