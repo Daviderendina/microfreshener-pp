c6ecd415fabfb467c2cc24943969db1c05104a15
Davide Rendina <d.rendina2@campus.unimib.it>
2022-12-27 18:56:18 +0100

    Improved split services refactoring

--- project/solver/impl/split_services_refactoring.py
+++ project/solver/impl/split_services_refactoring.py
@@ -7,7 +7,7 @@ from microfreshener.core.model.nodes import Compute, Service
 
 from project.exporter.export_object import ExportObject
 from project.kmodel.kube_cluster import KubeCluster
-from project.report.report_msg import compute_object_not_found_msg
+from project.report.report_msg import compute_object_not_found_msg, cannot_refactor_model_msg
 from project.report.report_row import RefactoringStatus
 from project.solver.refactoring import RefactoringNotSupportedError, Refactoring
 
@@ -18,38 +18,57 @@ class SplitServicesRefactoring(Refactoring):
         super().__init__(cluster, model, REFACTORING_SPLIT_SERVICES)
 
     def apply(self, smell: Smell):
+        object_to_add = []
+        export_object_to_add = []
+        abort = False
+
         if not isinstance(smell, MultipleServicesInOneContainerSmell):
             raise RefactoringNotSupportedError(f"Refactoring {self.name} not supported for smell {smell.name}")
 
         compute_node: Compute = smell.node
-        compute_object = self.cluster.get_object_by_name(compute_node.name)
+        workload = self.cluster.get_object_by_name(compute_node.name)
 
-        if compute_object:
-            self.cluster.remove_object(compute_object) # ATTENZIONE: se non rimuove nulla fa sparire obj e basta TODO
-            self.model.delete_node(compute_node)
-
+        if workload:
             name_count = 1
-            for container in compute_object.containers.copy():
-                object_copy = copy.deepcopy(compute_object)
+            for container in workload.containers.copy():
+
+                object_copy = copy.deepcopy(workload)
                 object_copy.set_containers([container])
                 object_copy.data["metadata"]["name"] += f"_{name_count}"
 
-                self.cluster.add_object(object_copy)
-                self.cluster.add_export_object(ExportObject(object_copy, None))
+                if self._refactor_model(container, object_copy):
+                    object_to_add.append(object_copy)
+                    export_object_to_add.append(ExportObject(object_copy, None))
+                else:
+                    abort = True
 
                 name_count += 1
 
-                # Refactor model
-                service_node = self.model.get_node_by_name(container.fullname, Service) #TODO quì può essere none? Ocio
-                compute_node = Compute(object_copy.fullname)
+            if not abort:
+                self.cluster.remove_object(workload)
+                self.model.delete_node(compute_node)
+                for object in object_to_add:
+                    self.cluster.add_object(object)
+                for exp_object in export_object_to_add:
+                    self.cluster.add_export_object(exp_object)
 
-                self.model.add_node(compute_node)
-                self.model.add_deployed_on(source_node=service_node, target_node=compute_node)
-
-            self._add_report_row(smell, RefactoringStatus.SUCCESSFULLY_APPLIED)
-            return True
+                self._add_report_row(smell, RefactoringStatus.SUCCESSFULLY_APPLIED)
+                return True
+            else:
+                self._add_report_row(smell, RefactoringStatus.NOT_APPLIED, cannot_refactor_model_msg())
+                return False
         else:
             self._add_report_row(smell, RefactoringStatus.NOT_APPLIED, compute_object_not_found_msg(compute_node.name))
             return False
 
+    def _refactor_model(self, container, object_copy):
+        # Refactor model
+        service_node = self.model.get_node_by_name(container.typed_fullname)
+        if service_node:
+            compute_node = Compute(object_copy.typed_fullname)
 
+            self.model.add_node(compute_node)
+            self.model.add_deployed_on(source_node=service_node, target_node=compute_node)
+            return True
+
+        return False
